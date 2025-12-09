0c4638bf134e571daa4ec6d7c09afb79fedf077c
Cem Aksoylar <caksoylar@users.noreply.github.com>
2025-12-09 21:33:47 +0000

    Support + in url param

--- vis.py
+++ vis.py
@@ -175,6 +175,7 @@ def main() -> None:
         st.query_params.clear()
         st.rerun()
     elif layout_cpt := st.query_params.get("cpt"):
+        layout_cpt = layout_cpt.replace(" ", "+")
         state.layouts = ortho_to_layouts(ortho_layout=None, cols_thumbs_notation=layout_cpt, split_gap=1.0)
         print("0.0 read cpt from query params")
         st.query_params.clear()
